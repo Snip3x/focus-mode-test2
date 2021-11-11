acf787664cedc032fcba10cecff90508f4389ce8
Snip3x <Bilawalimdad25blast@gmail.com>
2021-11-11 10:39:59 +0500

    main

--- public/main.js
+++ public/main.js
@@ -1,12 +1,26 @@
 const electron = require('electron');
-const app = electron.app;
+const { app, globalShortcut } = require('electron')
 const BrowserWindow = electron.BrowserWindow;
+
+
 
 const path = require('path');
 const url = require('url');
 const isDev = require('electron-is-dev');
 
 let mainWindow;
+app.on('ready', ()=>{
+
+  createWindow()});
+
+app.on('browser-window-blur',()=>{
+  console.log("ssssS")
+})
+
+app.on('browser-window-focus',()=>{
+  console.log("yeess")
+})
+
 
 function createWindow() {
   mainWindow = new BrowserWindow({width: 900, height: 680, fullscreen:true, focusable:true});
@@ -14,13 +28,46 @@ function createWindow() {
   mainWindow.on('closed', () => mainWindow = null);
 }
 
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  // Register a 'CommandOrControl+X' shortcut listener.
+  let ret = globalShortcut.register('ALT+TAB', () => {
+    console.log('CommandOrControl+X is pressed')
+  })
+
+  if (!ret) {
+    console.log('registration failed')
+  }
+
+  ret = globalShortcut.register('F11', () => {
+    console.log('CommandOrControl+X222 is pressed')
+  })
+
+  if (!ret) {
+    console.log('registration failed alt tab')
+  }
+
+  // Check whether a shortcut is registered.
+  console.log(globalShortcut.isRegistered('CommandOrControl+X'))
+})
+  app.on('blur',()=>console.log("sssss"))
+app.on('will-quit', () => {
+  // Unregister a shortcut.
+  globalShortcut.unregister('CommandOrControl+X')
+
+  // Unregister all shortcuts.
+  globalShortcut.unregisterAll()
+})
+
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
+
+
+
+
 
 app.on('activate', () => {
   if (mainWindow === null) {
