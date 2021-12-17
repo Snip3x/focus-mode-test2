d92c5cd3f9011633393364c6542f6b508cc6a621
Snip3x <Bilawalimdad25blast@gmail.com>
2021-12-17 07:59:19 +0500

    sds

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-const { app, globalShortcut } = require('electron')
+const {ipcMain, app, globalShortcut } = require('electron')
 const BrowserWindow = electron.BrowserWindow;
 const tasklist = require('win-tasklist');
 
@@ -13,38 +13,53 @@ const isDev = require('electron-is-dev');
 
 let mainWindow;
 app.on('ready', ()=>{
+    createWindow()
+  });
 
+
+ipcMain.on("exam-started",(e)=>{
   tasklist().then((list)=>{
   
     Object.entries(list).forEach(item=>{
       let name = item[1].process
-      if(name=='Discord.exe')
-      console.log(name)
-      check = false;
+      if(name=='steam.exe'){
+        console.log(name)
+        check = false;}
     })
   
     
-    createWindow()
-    if(check)
-    createWindow()
-    else
-    console.log("sssssnoooo")
+   
+    if(check){
+      e.reply("ok")
+      app.once('browser-window-blur',()=>{
+        console.log("Window has been force minimized Exam will be locked")
+        e.reply("exam-locked")
+      })
+      
+    
+      globalShortcut.register('F11', () => {
+        console.log('Fullscreen toggle has been disabled')
+      })
+    
+    }
   }).catch((err)=>{
     console.error(err);
   });
-  });
 
-app.on('browser-window-blur',()=>{
-  console.log("ssssS")
-})
-
-app.on('browser-window-focus',()=>{
-  console.log("yeess")
 })
 
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680, fullscreen:true, focusable:true});
+  mainWindow = new BrowserWindow({width: 900, 
+    height: 680, 
+    focusable:true,
+    webPreferences:{
+      nodeIntegration:true,
+      enableRemoteModule:true,
+      contextIsolation: false
+    }
+    
+  });
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('close', (e) => {
     // Do your control here
@@ -52,38 +67,21 @@ function createWindow() {
         e.preventDefault();
   
   })
+  mainWindow.on('alt+tab',(e)=>{
+    e.preventDefault();
+  })
   mainWindow.on('closed', () => mainWindow = null);
 }
 
-app.whenReady().then(() => {
-  // Register a 'CommandOrControl+X' shortcut listener.
-  let ret = globalShortcut.register('ALT+TAB', () => {
-    console.log('CommandOrControl+X is pressed')
-  })
-  
 
-  if (!ret) {
-    console.log('registration failed')
-  }
 
-  ret = globalShortcut.register('F11', () => {
-    console.log('CommandOrControl+X222 is pressed')
-  })
-
-  if (!ret) {
-    console.log('registration failed alt tab')
-  }
-
-  // Check whether a shortcut is registered.
-  console.log(globalShortcut.isRegistered('CommandOrControl+X'))
-})
-  app.on('blur',()=>console.log("sssss"))
 app.on('will-quit', () => {
   // Unregister a shortcut.
   globalShortcut.unregister('CommandOrControl+X')
 
   // Unregister all shortcuts.
   globalShortcut.unregisterAll()
+  console.log("sdsds")
 })
 
 
--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
 
 import './App.css';
+import React, { useState } from 'react';
+const electron = window.require("electron");
+
+
 
 function App() {
+  const [p1, setp1] = useState("Press the button to start the exam");
+  const [p2, setp2] = useState("After Pressing the button you will be in focus mode");
+
+  function startExam(){
+    const window = electron.remote.getCurrentWindow();
+
+
+    electron.ipcRenderer.send("exam-started")
+    electron.ipcRenderer.once("ok",()=>{
+      setp1("You are in focus mode window ALT+TAB and CTRL+ALT+DEL will Close your exam.");
+      setp2("Also ALT+F4 is disabled");
+    })
+
+    electron.ipcRenderer.once("exam-locked",()=>{
+      setp1("Exam has been locked");
+      setp2("Please contact your teacher");
+    })
+    window.setFullScreen(true);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          You are in focus mode window ALT+TAB and CTRL+ALT+DEL will Close your exam.
+          {p1}
         </p>
       <p>
-
-      Also ALT+F4 is disabled
+        {p2}
       </p>
+      <Welcome start={startExam} />
       </header>
     </div>
   );
 }
+
+function Welcome(props) {
+  return <button onClick={props.start}>start</button>;
+}
+
+
 
 export default App;
